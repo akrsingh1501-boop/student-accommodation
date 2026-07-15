5b7fb0ed7a7bbafed777fe96abe3f9c8d597f831
akrsingh1501-boop <akrsingh1501@gmail.com>
2026-07-15 15:14:58 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+  alert("Welcome! Aapki website kaam kar rahi hai.");
+});
