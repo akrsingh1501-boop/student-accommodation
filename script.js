ad7c5adde2f19c87e9f5bdd34589afac4f163394
akrsingh1501-boop <akrsingh1501@gmail.com>
2026-07-16 12:45:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ button.addEventListener("click", function (event) {
   event.preventDefault();
   alert("Welcome! Aapki website kaam kar rahi hai.");
 });
+fetch('fetch_properties.php')
+    .then(response => response.json())
+    .then(properties => {
+        console.log(properties);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
